ee2532fc2c37df3af023cb35b8b07182a46229e5
Pedro Silva <pdx.henrique@gmail.com>
2023-05-09 17:26:56 -0300

    add get-it as service container

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:logger_plus/logger_plus.dart';
 
 import 'app.dart';
+import 'di.dart';
 
 void main() async {
   await runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
+
+      unawaited(loadModules());
 
       runApp(const MyApp());
     },
